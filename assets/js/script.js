2392010aae841686ee516260abab3f533219bed8
douglas <douglasmaxton@gmail.com>
2023-09-10 11:00:08 +0100

    Setup structure for game functionality

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function runGame() {}
+
+function checkAnswer() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestions() {}
+
+function displaySubtractQuestions() {}
+
+function displayMultiplyQuestions() {}
+
+function displayDivideQuestions() {}
